ab4456af0d5e4ae5f7057d656d7f90fcab38a63d
Cristhian Osorio Avalos <32414969+cristhian1209@users.noreply.github.com>
2017-10-14 09:44:25 -0500

    Add files via upload

---
+++ PatronFactory/ConselePatronFactory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+
+namespace ConselePatronFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory Logger = new LoggerFactory();
+ 
+
+        }
+    }
+}
